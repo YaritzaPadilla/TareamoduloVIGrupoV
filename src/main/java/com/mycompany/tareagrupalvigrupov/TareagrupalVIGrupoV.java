0215bbdfd0e33bb95079d1825396d0c4c4840b1f
YaritzaPadilla <yaritzapadilla8@gmail.com>
2025-03-03 22:51:19 -0600

    En grupo

--- src/main/java/com/mycompany/tareagrupalvigrupov/TareagrupalVIGrupoV.java
+++ src/main/java/com/mycompany/tareagrupalvigrupov/TareagrupalVIGrupoV.java
@@ -8,7 +8,7 @@ import helpers.Deportista;
 public class TareagrupalVIGrupoV {
 
    public static void main(String[] args) {
-        Doctor doctorM = new Doctor(" Deneel Mauricio "," Choluteca ", 40," Ortopeda ");
+        Doctor doctorM = new Doctor(" Dannel Mauricio "," Choluteca ", 40," Ortopeda ");
         Deportista deportistaN = new Deportista(" Cesar Rivera "," Choloma ", 24, " Tenis ");
 
         Persona[] personas = {doctorM, deportistaN};
